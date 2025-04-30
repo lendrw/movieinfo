0824299dd22a72e6b4854b0513beef5eea8e16ac
Leandro Freire <88214669+lendrw@users.noreply.github.com>
2025-04-30 17:03:10 -0300

    Revert "fixing route issues"
    This reverts commit 8414075618340036053cd58e7bf102001f508451.

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/movieinfo/',
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'dist/index.html',
-          dest: 'dist',
-          rename: '404.html',
-        }
-      ]
-    })
-  ],
+  plugins: [react()],
 })
