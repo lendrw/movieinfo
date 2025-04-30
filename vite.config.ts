8414075618340036053cd58e7bf102001f508451
Leandro Freire <88214669+lendrw@users.noreply.github.com>
2025-04-30 16:50:23 -0300

    fixing route issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/movieinfo/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'dist/index.html',
+          dest: 'dist',
+          rename: '404.html',
+        }
+      ]
+    })
+  ],
 })
